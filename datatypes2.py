ace497c359788bd586741c06f54d0c10930e8813
Shubham <40167163+shubhamthorat07@users.noreply.github.com>
2020-07-15 21:13:47 +0530

    Add files via upload

---
+++ datatypes2.py
@@ -0,0 +1,19 @@
+t = (10,20,"Python")
+
+print(t)
+print(id(t))
+print(type(t))
+
+
+
+d = {"Name" : "Jack", "Age" : "30", "Gender" : "Male"}
+print(d)
+print(id(d))
+
+
+s = {10,20}
+print(s)
+
+b = True
+print(b)
+print(type(b))
